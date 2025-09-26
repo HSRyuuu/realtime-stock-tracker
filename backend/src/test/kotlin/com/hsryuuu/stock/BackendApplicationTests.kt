6ebaa24e4453a292be2287025456f34f35d2ca9a
happyhsryu <happyhsryu@gmail.com>
2025-09-27 01:49:13 +0900

    Backend initial commit

---
+++ backend/src/test/kotlin/com/hsryuuu/stock/BackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hsryuuu.stock
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
